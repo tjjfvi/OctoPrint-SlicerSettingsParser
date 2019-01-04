324ca452d49094ef22f146cb33101d0abde6c04e
tjjfvi <44031566+tjjfvi@users.noreply.github.com>
2019-01-04 14:05:59 -0500

    vb to 2.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_SlicerSettingsParser"
 plugin_name = "OctoPrint-SlicerSettingsParser"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.0.1"
+plugin_version = "2.0.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
